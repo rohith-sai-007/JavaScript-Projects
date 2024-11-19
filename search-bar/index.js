b091a34efaea9234467521889f9f0ca7916b6fb8
mohith1399 <mohithku@buffalo.edu>
2024-11-19 11:05:56 -0500

    All

--- search-bar/index.js
+++ search-bar/index.js
@@ -1,26 +1,7 @@
-const imageContainerEl = document.querySelector(".images-fold");
+const searchBarContainerEl = document.querySelector(".search-bar-container");
 
-const prevEl = document.getElementById("prev");
-const nextEl = document.getElementById("next");
-let x = 0;
-let timer;
-prevEl.addEventListener("click", () => {
-  x = x + 45;
-  clearTimeout(timer);
-  updateGallery();
+const magnifierEl = document.querySelector(".magnifier");
+
+magnifierEl.addEventListener("click", () => {
+  searchBarContainerEl.classList.toggle("active");
 });
-nextEl.addEventListener("click", () => {
-  x = x - 45;
-  clearTimeout(timer);
-  updateGallery();
-});
-
-function updateGallery() {
-  imageContainerEl.style.transform = `perspective(1000px) rotateY(${x}deg)`;
-  timer = setTimeout(() => {
-    x = x - 45;
-    updateGallery();
-  }, 3000);
-}
-
-updateGallery();
